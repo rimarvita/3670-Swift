ec0e746e7bb0f07d13b4ddb7578d811ae8ca2024
SBabchukTM <babchuk.serhii@trident-media.agency>
2025-10-31 11:59:07 +0200

    init

---
+++ CocoWars/App/CocoWarsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CocoWarsApp.swift
+//  CocoWars
+//
+//  Created by Serhii Babchuk on 31.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct CocoWarsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            CWLaunchView()
+        }
+    }
+}
